9e90bec3c46cbb4b711d8182477665e48c0a61da
Roberto Bellingeri <bellingeri@netguru.it>
2019-03-19 12:25:39 +0100

    Update settings.php

--- lang/it/settings.php
+++ lang/it/settings.php
@@ -1,9 +1,8 @@
 <?php
 /**
- * File di lingua Italiana per la pagina di configurazione
+ * Italian language file for config
  *
  */
-
 
 $lang['sidebar_sections']   = 'Attiva/disattiva la barra laterale nelle sezioni su questi elementi:';
 $lang['sidebar_sections_o_h1']   = 'Intestazioni di primo ordine';
@@ -12,6 +11,5 @@ $lang['sidebar_sections_o_h1,h2,h3,h4,h5']   = 'Tutte le intestazioni';
 $lang['sidebar_sections_o_li.level1 > .li']   = 'Primo livello degli elementi di elenco';
 
 $lang['header_layout']   = 'Regola il layout dell\'intestazione:';
-$lang['header_layout_o_header-default']   = 'layout di intestazione generoso per un lungo titolo e una tagline';
-$lang['header_layout_o_header-compact']   = 'layout di intestazione compatto per un breve titolo e tagline nascosta';
-
+$lang['header_layout_o_header-default']   = 'Layout di intestazione generoso per un lungo titolo e una tagline';
+$lang['header_layout_o_header-compact']   = 'Layout di intestazione compatto per un breve titolo e tagline nascosta';
